9538333adbd9e0064bba9afc4629223fd8748ebd
mr-krystian-stawarczyk <mr.krystian.stawarczyk@gmail.com>
2024-09-20 16:06:26 +0200

    web4

--- next.config.js
+++ next.config.js
@@ -1,9 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
 	reactStrictMode: true,
-	trailingSlash: true, // Ensures that URLs end with a trailing slash for static export
-	output: "export", // Tells Next.js to export as static HTML
-	distDir: "out", // Output directory for static files
+	// Output directory for static files
 };
 
 module.exports = nextConfig;
